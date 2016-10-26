560545796ad4b0cb107542d64c16c23667f7bfaf
MaxAhnfeldt <maximilian.ahnfeldt@nordakademie.de>
2016-10-26 12:59:35 +0200

    blabla

--- src/uebung_3_2/uebung_3_2.java
+++ src/uebung_3_2/uebung_3_2.java
@@ -4,73 +4,64 @@ public class uebung_3_2 {
 
 	public static void main(String[] args) {
 
-		double bestellwert = 500;
-		double bisherigesKundenBestellvolumen = 250;
+		double bestellwert = 25000;
+		double bisherigesKundenBestellvolumen = 12500;
 		boolean istPremiumKunde = true;
 
 		double mengenrabatt = 0;
-		double mengenrabatt0 = 0;
-		double mengenrabatt1 = 1;
-		double mengenrabatt2 = 2;
-		double mengenrabatt3 = 3;
-		double mengenrabatt4 = 5;
+		double treuerabatt = 0;
+		double bonusrabatt = 0;
+		double rabatt = 0;
 
-		if (bestellwert >= 100 && bestellwert < 500) {
-			mengenrabatt = mengenrabatt1;
+		System.out.println("Bestellwert: " + bestellwert);
+
+		if (bestellwert >= 5000) {
+			mengenrabatt = 5;
 		} else {
-			if (bestellwert >= 500 && bestellwert < 2000) {
-				mengenrabatt = mengenrabatt2;
+			if (bestellwert >= 2000) {
+				mengenrabatt = 3;
 			} else {
-				if (bestellwert >= 2000 && bestellwert < 5000) {
-					mengenrabatt = mengenrabatt3;
+				if (bestellwert >= 500) {
+					mengenrabatt = 2;
 				} else {
-					if (bestellwert >= 5000) {
-						mengenrabatt = mengenrabatt4;
+					if (bestellwert >= 100) {
+						mengenrabatt = 1;
+					} else {
+						mengenrabatt = 0;
 					}
 				}
 			}
 		}
-		if (istPremiumKunde = true) {
-			mengenrabatt0 = 2 * mengenrabatt;
-		} else {
+		if (istPremiumKunde == true) {
+			mengenrabatt = 2 * mengenrabatt;
 		}
-
-		double treuerabatt = 0;
-		double treuerabatt1 = 7.5;
-		double treuerabatt2 = 12.5;
 
 		if (bisherigesKundenBestellvolumen > 50000) {
-			treuerabatt = treuerabatt2;
+			treuerabatt = 12.5;
 		} else {
 		}
-		if (bisherigesKundenBestellvolumen > 10000 && bisherigesKundenBestellvolumen <= 50000) {
-			treuerabatt = treuerabatt1;
+		if (bisherigesKundenBestellvolumen > 10000) {
+			treuerabatt = 7.5;
 		} else {
+			treuerabatt = 0;
 		}
-		double bonusrabatt = 0;
-		double bonusrabatt1 = 3;
 
 		if (bestellwert >= 2 * bisherigesKundenBestellvolumen) {
-			bonusrabatt = bonusrabatt1;
+			bonusrabatt = 3;
 		} else {
 			bonusrabatt = 0;
 		}
-		double rabatt;
-		rabatt = mengenrabatt0 + treuerabatt + bonusrabatt;
+		rabatt = mengenrabatt + treuerabatt + bonusrabatt;
 
 		if (rabatt > 20 && istPremiumKunde == true) {
-			System.out.println("Rabatt: 20%");
-		} else {
-			if (rabatt <= 20 && istPremiumKunde == true) {
-				System.out.println("Rabatt:" + rabatt);
-			} else {
-				if (rabatt > 15 && istPremiumKunde == false) {
-					System.out.println("Rabatt: 15%");
-				} else {
-					System.out.println("Rabatt: " + rabatt);
-				}
-			}
+			rabatt = 20;
+		} 
+
+		if (rabatt > 15 && istPremiumKunde == false) {
+			rabatt = 15;
 		}
 
+		System.out.println("Rabatt: " + rabatt);
+		System.out.println("rabattierter Bestellwert: " + (bestellwert * (100 - rabatt) / 100));
 	}
 }
