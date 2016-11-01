7f7856d45797ba853671332f76c3fbfbfbd63432
14696 <14696@C110-02.nordakademie.intern>
2016-11-01 15:46:07 +0100

    Ich hänge noch bei der Aufgabe vom Kundenrabatt.
    Es wird mir ein Rabatt und der Bestellt wert auch schon ausgeworfen,
    jedoch kann dieser nicht sein. Leider werde ich durch blick in der
    Beispiellösung nicht schlauer, können Sie mir sagen, wo der Fehler
    liegt?
    
    Gruß 
    Finnja Kietzke

--- src/uebung_3_2/uebung_3_2.java
+++ src/uebung_3_2/uebung_3_2.java
@@ -4,46 +4,45 @@ public class uebung_3_2 {
 
 	public static void main(String[] args) {
 		
-		double bestellwert = 1;
-		double bisherigesKundenBestellvolumen =  2;
-		boolean istPremiumKunde = true ;
-		double rabatt;
-		double rabatt2;
+		double bestellwert = 10000;
+		double bisherigesKundenBestellvolumen =  0;
+		boolean istPremiumKunde = false ;
+		double rabatt = 0; 
+		
 		
 		// Mengenrabatt
 		
 		if (bestellwert >= 5000) {
-			rabatt = 0.05; } else  {
-			if (bestellwert >=200) {
-				rabatt = 0.03; } else {
-					if (bestellwert >= 500) {
-						rabatt = 0.02; } else {
-							if  (bestellwert >= 100) {
-								rabatt = 0.01; } else {
-							}
-					}
-			}
-		}
+			rabatt = 0.05; } else if (bestellwert >=200) {
+				rabatt = 0.03; } else if (bestellwert >= 500) {
+						rabatt = 0.02; } else if (bestellwert >= 100) {
+								rabatt = 0.01; } 
+						
+		
 		
 		// Premiumkunden Doppel
 		
-		if (istPremiumKunde = true) {rabatt=rabatt*2; } else {};
+		if (istPremiumKunde) {rabatt=rabatt*2; }
 		
 		// Rabatt nach Bestellvolumen
 		
 		if (bisherigesKundenBestellvolumen >= 50000) {
-			rabatt2 = 0.125; } else {
-				if (bisherigesKundenBestellvolumen >= 10000) { rabatt2 = 0.075; } else {}
+			rabatt = rabatt + 0.125; } 
+		else if (bisherigesKundenBestellvolumen >= 10000) { rabatt = rabatt + 0.075; }
 				
-		}
+		if (bestellwert >= ( 2* bisherigesKundenBestellvolumen)) { rabatt = rabatt + 0.03; }
 		
 		// Rabatt Eingrenzung
-		if 
+	
+		if (istPremiumKunde && (rabatt > 0.2)) { rabatt = 0.20;}
+		if ((!istPremiumKunde) && (rabatt > 0.15)) { rabatt = 0.15;}
 		
 		
-		System.out.println(rabatt);
+		double rabattInProzent = rabatt * 100;
+		
+		System.out.println(rabattInProzent);
 		System.out.println(bestellwert);
-		System.out.println(bestellwertrabatt);
 		
-}
+	
+	}		
 }
